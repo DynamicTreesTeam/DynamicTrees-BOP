1b3ba56e392f06d8db7271af8ce4637a952eee4d
ferreusveritas <ferreusveritas@gmail.com>
2018-03-08 11:14:59 -0500

    Abstract model loader

--- src/main/java/dynamictreesbop/models/ModelLoaderBamboo.java
+++ src/main/java/dynamictreesbop/models/ModelLoaderBamboo.java
@@ -1,41 +1,28 @@
 package dynamictreesbop.models;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.function.Function;
 
-import org.apache.commons.io.IOUtils;
-
 import com.ferreusveritas.dynamictrees.models.BranchModel;
-import com.ferreusveritas.dynamictrees.models.ModelLoaderBranch;
-import com.google.common.base.Charsets;
+import com.ferreusveritas.dynamictrees.models.ModelLoaderGeneric;
 
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelBlock;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
-import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.model.IModelState;
 
-public class ModelLoaderBamboo extends ModelLoaderBranch {
-
-public final String code = "#dynamicbamboo";
+public class ModelLoaderBamboo extends ModelLoaderGeneric {
 	
-	// return true if our Model Loader accepts this ModelResourceLocation
-	@Override
-	public boolean accepts(ResourceLocation resourceLocation) {
-		return resourceLocation.getResourcePath().endsWith(code); // Hacky but fast
+	public ModelLoaderBamboo() {
+		super("dynamicbamboo", new ResourceLocation("dynamictrees", "block/smartmodel/branch"));
 	}
 	
-	// When called for our BlockBranch's ModelResourceLocation, return our BranchModel.
 	@Override
-	public IModel loadModel(ResourceLocation resourceLocation) {
-		ModelBlock modelBlock = getBranchBlockModel(resourceLocation); // We need this to model textures.
-		return (modelBlock != null) ? new BranchModel(modelBlock) {
+	protected IModel loadModel(ResourceLocation resourceLocation, ModelBlock baseModelBlock) {
+		return new BranchModel(baseModelBlock) {
 			@Override
 			public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter) {
 				try {
@@ -45,49 +32,7 @@ public final String code = "#dynamicbamboo";
 					return ModelLoaderRegistry.getMissingModel().bake(state, format, bakedTextureGetter);
 				}
 			}
-		} : ModelLoaderRegistry.getMissingModel();
+		};
 	}
 	
-	@Override
-	protected ModelBlock getBranchBlockModel(ResourceLocation virtualLocation) {
-		if (!accepts(virtualLocation)) {
-			return null;
-		}
-		
-		String path = virtualLocation.getResourcePath(); // Extract the path portion of the ResourceLocation
-		path = path.substring(0, path.length() - code.length()); // Remove the ending code from the location
-		ResourceLocation location = new ResourceLocation(virtualLocation.getResourceDomain(), path); // Recreate the resource location without the code
-		
-		ModelBlock modelBlock = null;
-		Reader reader = null;
-		IResource iresource = null;
-		
-		try {
-			iresource = this.resourceManager.getResource(this.getModelLocation(location));
-			reader = new InputStreamReader(iresource.getInputStream(), Charsets.UTF_8);
-			modelBlock = ModelBlock.deserialize(reader);
-			modelBlock.name = location.toString();
-			
-			ModelBlock rootParent = modelBlock;
-			
-			//Climb the hierarchy to discover the name of the root parent model
-			while (rootParent.parent != null) {
-				rootParent = rootParent.parent;
-			}
-			
-			// If the name of the parent node is our model then we're good to go.
-			if (rootParent.getParentLocation() != null && rootParent.getParentLocation().toString().equals(BRANCH_MODEL_RESOURCE_NAME)) {
-				return modelBlock;
-			}
-			
-			return null;
-			
-		} catch (IOException e) {
-			IOUtils.closeQuietly(reader);
-			IOUtils.closeQuietly(iresource);
-		}
-		
-		return null;
-	}
-
 }
