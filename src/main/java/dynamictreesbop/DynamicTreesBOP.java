5dcf70769b9a3fe5f9536e9012b46642db99fe0d
the-realest-stu <stuartlindstrom@gmail.com>
2018-05-07 15:57:35 -0400

    Updated version number

--- src/main/java/dynamictreesbop/DynamicTreesBOP.java
+++ src/main/java/dynamictreesbop/DynamicTreesBOP.java
@@ -17,7 +17,7 @@ public class DynamicTreesBOP {
 	
 	public static final String MODID = "dynamictreesbop";
 	public static final String NAME = "Dynamic Trees BOP";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0a";
 	public static final String DEPENDENCIES = "required-after:dynamictrees@[1.12.2-0.7.7,);required-after:biomesoplenty@[7.0.1.2344,)";
 	
 	@Mod.Instance
