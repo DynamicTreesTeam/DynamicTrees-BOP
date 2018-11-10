839712c277ef91b5ba632bb039dfcb3285650810
ferreusveritas <ferreusveritas@gmail.com>
2018-11-10 18:57:53 -0500

    Make build more automated

--- src/main/java/dynamictreesbop/ModConstants.java
+++ src/main/java/dynamictreesbop/ModConstants.java
@@ -4,8 +4,7 @@ public class ModConstants extends com.ferreusveritas.dynamictrees.ModConstants {
 	
 	public static final String MODID = "dynamictreesbop";
 	public static final String NAME = "Dynamic Trees BOP";
-	public static final String VERSIONAUTO = "@VERSION@";
-	public static final String VERSION = VERSIONDEV;//Change to VERSIONDEV in development, VERSIONAUTO for release
+	public static final String VERSION = "1.12.2-9999.9999.9999z";//Maxed out version to satisfy dependencies during dev, Assigned from gradle during build, do not change
 	
 	//Other Mods
 	public static final String BOP = "biomesoplenty";
