7fa3cb685918cb47f833a13b7f5935ccf0417802
ferreusveritas <ferreusveritas@gmail.com>
2018-11-27 09:42:34 -0500

    Flowering vines replaced with Ivy for BoP change.

--- src/main/java/dynamictreesbop/trees/species/SpeciesOakFloweringVine.java
+++ src/main/java/dynamictreesbop/trees/species/SpeciesOakFloweringVine.java
@@ -43,7 +43,7 @@ public class SpeciesOakFloweringVine extends Species {
 		setupStandardSeedDropping();
 		
 		//Add species features
-		addGenFeature(new FeatureGenVine(this).setQuantity(12).setMaxLength(6).setRayDistance(6).setVineBlock(BOPBlocks.flower_vine));//Generate Vines
+		addGenFeature(new FeatureGenVine(this).setQuantity(12).setMaxLength(6).setRayDistance(6).setVineBlock(BOPBlocks.ivy));//Generate Ivy
 	}
 	
 	@Override
