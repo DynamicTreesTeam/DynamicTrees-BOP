9c6762a88af7529d983ea7e59f0dae5c7862ab61
supermassimo <massimocantugarcia@gmail.com>
2021-06-03 00:14:42 -0300

    added tropics biome worldgen

--- src/main/java/therealeststu/dtbop/DTBOPRegistries.java
+++ src/main/java/therealeststu/dtbop/DTBOPRegistries.java
@@ -1,15 +1,25 @@
 package therealeststu.dtbop;
 
+import biomesoplenty.api.block.BOPBlocks;
 import com.ferreusveritas.dynamictrees.api.cells.CellKit;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
 import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
+import com.ferreusveritas.dynamictrees.blocks.rootyblocks.RootyBlock;
+import com.ferreusveritas.dynamictrees.systems.DirtHelper;
+import com.ferreusveritas.dynamictrees.systems.RootyBlockHelper;
+import com.ferreusveritas.dynamictrees.systems.dropcreators.FruitDropCreator;
+import com.ferreusveritas.dynamictrees.trees.Mushroom;
 import com.ferreusveritas.dynamictrees.trees.Species;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 import therealeststu.dtbop.cells.DTBOPCellKits;
+import therealeststu.dtbop.trees.Bush;
 import therealeststu.dtbop.trees.PoplarSpecies;
 import therealeststu.dtbop.trees.TwigletSpecies;
 
@@ -34,8 +44,24 @@ public class DTBOPRegistries {
     }
 
     @SubscribeEvent
-    public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+    public static void registerSpecies (final com.ferreusveritas.dynamictrees.api.registry.RegistryEvent<Species> event) {
+        // Registers fake species for generating bushes.
+        event.getRegistry().registerAll(new Bush("flowering_oak_bush", new ResourceLocation("oak_log"), new ResourceLocation("oak_leaves"), new ResourceLocation("biomesoplenty", "flowering_oak_leaves")));
+    }
 
+    @SubscribeEvent
+    public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+        Bush.INSTANCES.forEach((Bush::setup));
+        
+        DirtHelper.registerSoil(BOPBlocks.origin_grass_block, DirtHelper.DIRT_LIKE);
+        DirtHelper.registerSoil(BOPBlocks.white_sand, DirtHelper.SAND_LIKE);
+        DirtHelper.registerSoil(BOPBlocks.orange_sand, DirtHelper.SAND_LIKE);
+        DirtHelper.registerSoil(BOPBlocks.black_sand, DirtHelper.SAND_LIKE);
+        DirtHelper.registerSoil(BOPBlocks.dried_salt, DirtHelper.SAND_LIKE);
+        DirtHelper.registerSoil(BOPBlocks.mud, DirtHelper.MUD_LIKE);
+
+        for (RootyBlock rooty : RootyBlockHelper.generateListForRegistry(true, DynamicTreesBOP.MOD_ID))
+            event.getRegistry().register(rooty);
     }
 
 }
---
+++ src/main/java/therealeststu/dtbop/trees/Bush.java
@@ -0,0 +1,53 @@
+package therealeststu.dtbop.trees;
+
+import com.ferreusveritas.dynamictrees.DynamicTrees;
+import com.ferreusveritas.dynamictrees.systems.genfeatures.BushGenFeature;
+import com.ferreusveritas.dynamictrees.systems.genfeatures.GenFeatures;
+import com.ferreusveritas.dynamictrees.trees.Species;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+import therealeststu.dtbop.DynamicTreesBOP;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Bush extends Species {
+
+    public static List<Bush> INSTANCES = new LinkedList<>();
+
+    ResourceLocation log,leaves,altLeaves;
+
+    public Bush(String name, ResourceLocation log, ResourceLocation leaves) {
+       this(name, log, leaves,null);
+    }
+    public Bush(String name, ResourceLocation log, ResourceLocation leaves, ResourceLocation altLeaves) {
+        this.setRegistryName(new ResourceLocation(DynamicTreesBOP.MOD_ID, name));
+        this.setUnlocalizedName(this.getRegistryName().toString());
+        this.setStandardSoils();
+        this.log = log;
+        this.leaves = leaves;
+        this.altLeaves = altLeaves;
+
+        INSTANCES.add(this);
+    }
+
+    public void setup(){
+        Block logBlock = ForgeRegistries.BLOCKS.getValue(log);
+        Block leavesBlock = ForgeRegistries.BLOCKS.getValue(leaves);
+        Block altLeavesBlock = null;
+        if (altLeaves != null){
+            Block altLeafBlock = ForgeRegistries.BLOCKS.getValue(altLeaves);
+            if (altLeafBlock != Blocks.AIR) altLeavesBlock = altLeafBlock;
+        }
+        this.addGenFeature(GenFeatures.BUSH.with(BushGenFeature.LOG_BLOCK, logBlock)
+                .with(BushGenFeature.LEAVES_BLOCK, leavesBlock).with(BushGenFeature.SECONDARY_LEAVES_BLOCK, altLeavesBlock));
+    }
+
+    @Override
+    public boolean isTransformable() {
+        return false;
+    }
+
+}
